8d1a1916bf6f762dc797d6fcd5070cc1df7102d6
Andre Teixeira <andre.teixeira@gmail.com>
2018-04-04 17:35:48 +0200

    Moving Login and Main components to containers folder

---
+++ src/Containers/Styles/MainScreenStyles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+import { Colors, Metrics } from '../../Themes';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: '#71BAB3'
+    },
+    user_card__container: {
+        flex: 1,
+    },
+    content__container: {
+        flex: 10,
+    },
+    content__footer: {
+        flex: 11,
+    },
+});
