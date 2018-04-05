01f0bedd425bb651c674e9bdda42271f55891005
Andre Teixeira <andre.teixeira@gmail.com>
2018-04-05 16:58:23 +0200

    finishing login reducer tests

--- __tests__/Redux/Login.tests.js
+++ __tests__/Redux/Login.tests.js
@@ -14,9 +14,39 @@ describe('Login reducer', () => {
     });
   })
 
-  it('Should set the fetching flag properly on request', () => {
-    const state = defaultState(Actions.loginRequest('u', 'p'));
+  it('sets the fetching flag properly on request', () => {
+    const action = Actions.loginRequest('u', 'p');
+    const state = defaultState(action);
+
     expect(state.fetching).toBe(true);
   });
+
+  it('sets state properly on login success', () => {
+    const action = Actions.loginSuccess('success', '7B967T976G976G7687B97SDAFS=');
+    const state = defaultState(action);
+
+    expect(state.username).toBe('success');
+    expect(state.fetching).toBe(false);
+    expect(state.token).toBe('7B967T976G976G7687B97SDAFS=');
+  });
+
+  it('sets state properly on login failure', () => {
+    const action = Actions.loginFailure('Error message');
+    const state = defaultState(action);
+
+    expect(state.username).toBe(null);
+    expect(state.fetching).toBe(false);
+    expect(state.token).toBe(null);
+    expect(state.error).toBe('Error message');
+  });
+
+  test('isLoggedIn selector must be truthy on login success', () => {
+    const action = Actions.loginSuccess('success')
+    const state = defaultState(action);
+
+    expect(isLoggedIn(state)).toBe(true)
+  })
+
+  
 
 });
